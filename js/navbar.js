e701b88590cf775e70b984a4a3ee35d80355baed
Juan Carlo David <jcdavid@Juans-MacBook-Air.local>
2024-04-05 22:33:13 +0800

    home page completed

--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,15 @@
 const bar = document.getElementById("bar");
 const list = document.querySelector('.left');
 let count = 0
-bar.addEventListener("click", ()=>{
-    if(count === 0){
-        list.style.height = '125px';
-        count++
-    }else{
-        list.style.height = '0';
-        count--;
-    }
-})
+
+if(bar){
+    bar.addEventListener("click", ()=>{
+        if(count === 0){
+            list.style.height = '125px';
+            count++
+        }else{
+            list.style.height = '0';
+            count--;
+        }
+    })
+}
